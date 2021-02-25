3b976550de494f473adc08848e76c735cf967555
gomezago <70365962+gomezago@users.noreply.github.com>
2021-02-25 16:31:13 +0100

    Add fields to cause model

--- causes/models.py
+++ causes/models.py
@@ -3,11 +3,15 @@ from django.db import models
 # Create your models here.
 class Cause(models.Model):
     title = models.CharField(max_length=100, blank=False) # Short string frield to hold the name of the cause
-    description = models.TextField(blank=False) # Large string frield to hold a piece of text
-    data = models.CharField(max_length=50, blank=False) # Short string
-    #image = models.FilePathField(path="/img") # Image field, holds the path where the image is stored
+    description_tweet = models.CharField(max_length=280, blank=False, null=True) #Tweet-Like description of project
+    description = models.TextField(blank=False, null=True) # Large string frield to hold a piece of text
+    data = models.CharField(max_length=100, blank=False) # Short string
     image = models.ImageField(upload_to="images", blank=True)
-    active = models.BooleanField(default=False)
+    active = models.BooleanField(default=False) # Status: Accepting data or not
+    start = models.DateField(blank=False, null=True) # Start of project
+    end = models.DateField(blank=False, null=True) # End of project
+    contact_name = models.CharField(max_length=100, blank=False, null=True) #Name of Researcher
+    contact_email = models.EmailField(blank=False, null=True) # Email of person in charge.
 
     def __str__(self):
         return self.title
