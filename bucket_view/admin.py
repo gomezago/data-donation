e8006e7a9fceb78f64ee981252206ed5bc6698a3
gomezago <70365962+gomezago@users.noreply.github.com>
2021-09-23 10:45:25 +0200

    Add Donation model to admin

--- bucket_view/admin.py
+++ bucket_view/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Donation
 # Register your models here.
 class ProjectAdmin(admin.ModelAdmin):
     pass
 
+class DonationAdmin(admin.ModelAdmin):
+    pass
+
 admin.site.register(Project,ProjectAdmin)
+admin.site.register(Donation, DonationAdmin)
