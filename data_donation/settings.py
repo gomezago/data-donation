687689b5f6694ccc7bcc1ffbc22cad75964b83ae
gomezago <70365962+gomezago@users.noreply.github.com>
2022-01-28 11:43:34 +0100

    Setup celery

--- data_donation/settings.py
+++ data_donation/settings.py
@@ -208,3 +208,12 @@ LOGGING = {
 
 # Email Backend
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+# CELERY STUFF
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Europe/Amsterdam'
